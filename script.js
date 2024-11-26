3c90c7123fb4e1c3d10519d51dec5c91db718f20
William <willspri@gmail.com>
2024-11-26 14:25:57 +1300

    added top light to functions

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 let gridItems = document.querySelectorAll('.grid-item');
 
-gridItems.forEach(gridItem => {
+
+gridItems.forEach((gridItem, index) => {
     gridItem.addEventListener('click', () => {
+   
         if (gridItem.style.backgroundImage === 'linear-gradient(purple, white, purple)') {
             gridItem.style.backgroundImage = 'linear-gradient(purple, yellow, purple)';
         } else {
             gridItem.style.backgroundImage = 'linear-gradient(purple, white, purple)';
+        }
+
+
+        if (index >= 5) {  
+            const aboveItem = gridItems[index - 5];
+           
+            if (aboveItem.style.backgroundImage === 'linear-gradient(purple, white, purple)') {
+                aboveItem.style.backgroundImage = 'linear-gradient(purple, yellow, purple)';
+            } else {
+                aboveItem.style.backgroundImage = 'linear-gradient(purple, white, purple)';
+            }
         }
     });
 });
