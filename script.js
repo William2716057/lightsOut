47ed2d489392e1560ef95d9ed548b8ac39c376bc
William <willspri@gmail.com>
2025-05-13 12:41:13 +1200

    win conditions

--- script.js
+++ script.js
@@ -34,24 +34,21 @@ gridItems.forEach((gridItem, index) => {
     });
 });
 
-// Function to toggle background image
-// Will function currectly without fading 
-//function toggleGridItem(gridItem) {
- //   if (gridItem.style.backgroundImage === 'linear-gradient(purple, white, purple)') {
- //       gridItem.style.backgroundImage = 'linear-gradient(purple, yellow, purple)';
-//    } else {
-//gridItem.style.backgroundImage = 'linear-gradient(purple, purple, purple)';
- //   }
-//}
-
 function toggleGridItem(gridItem) {
-    const currentBg = getComputedStyle(gridItem).backgroundImage;
+    const currentBack = getComputedStyle(gridItem).backgroundImage;
 
-    if (currentBg.includes('rgb(255, 255, 255)')) {
+    if (currentBack.includes('rgb(255, 255, 255)')) {
         // if square is yellow (on)
         gridItem.style.backgroundImage = 'linear-gradient(purple, yellow, purple)';
     } else {
         // if square is white (off)
         gridItem.style.backgroundImage = 'linear-gradient(purple, white, purple)';
     }
+ 
 }
+
+//function winCondition(gridItem) {
+//    if (rowLength = 'linear-gradient(purple, white, purple)')
+//    alert("You win!!!");
+
+//}
