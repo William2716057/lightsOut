e6d182905f3d15e7e99cfa5827c3fa53b66ac940
William <willspri@gmail.com>
2025-05-13 13:04:05 +1200

    optomised grid item conditions

--- script.js
+++ script.js
@@ -1,36 +1,42 @@
 let gridItems = document.querySelectorAll('.grid-item');
+const rowLength = 5;
 
 gridItems.forEach((gridItem, index) => {
     gridItem.addEventListener('click', () => {
 
         toggleGridItem(gridItem);
 
+        if (index - rowLength >= 0) toggleGridItem(gridItems[index - rowLength]);
+        if (index + rowLength < gridItems.length) toggleGridItem(gridItems[index + rowLength]);
+        if (index % rowLength !== 0) toggleGridItem(gridItems[index - 1]);
+        if ((index + 1) % rowLength !== 0) toggleGridItem(gridItems[index + 1]);
+
         // Get indices for neighbors (above, below, left, right)
-        const rowLength = 5; // 5 items per row
+       // const rowLength = 5; // 5 items per row
 
         // Check for item above (index - rowLength)
-        if (index - rowLength >= 0) {
-            const aboveItem = gridItems[index - rowLength];
-            toggleGridItem(aboveItem);
-        }
+       // if (index - rowLength >= 0) {
+      //      const aboveItem = gridItems[index - rowLength];
+      //      toggleGridItem(aboveItem);
+      //  }
 
         // Check for item below (index + rowLength)
-        if (index + rowLength < gridItems.length) {
-            const belowItem = gridItems[index + rowLength];
-            toggleGridItem(belowItem);
-        }
+      //  if (index + rowLength < gridItems.length) {
+      //      const belowItem = gridItems[index + rowLength];
+      //      toggleGridItem(belowItem);
+//}
 
         // Check the item to  left (index - 1), ensuring not the first column
-        if (index % rowLength !== 0) {
-            const leftItem = gridItems[index - 1];
-            toggleGridItem(leftItem);
-        }
+    //    if (index % rowLength !== 0) {
+    //        const leftItem = gridItems[index - 1];
+     //       toggleGridItem(leftItem);
+    //    }
 
         // Check for item to  right (index + 1), ensuring not the last column
-        if ((index + 1) % rowLength !== 0) {
-            const rightItem = gridItems[index + 1];
-            toggleGridItem(rightItem);
-        }
+     //   if ((index + 1) % rowLength !== 0) {
+       //     const rightItem = gridItems[index + 1];
+      //      toggleGridItem(rightItem);
+       // }
     });
 });
 
@@ -47,18 +53,5 @@ function toggleGridItem(gridItem) {
  
 }
 
-//function winCondition(gridItem) {
-//    if (rowLength = 'linear-gradient(purple, white, purple)')
-//    alert("You win!!!");
+//win condition function
 
-//}
-
-function windConditions() {
-    const allOff = Array.from(gridItems).every(item =>
-        toggleGridItem(item).backGroundIamege.includes('rgb(purple, white, purple)',
-        );
-
-    if (allOff) {
-        alert("You win");
-    }
-}
